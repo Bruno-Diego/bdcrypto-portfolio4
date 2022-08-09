704c3a850261aa3136f82df33782c5fff7355261
BrunoDS <bruno.diego.x@hotmail.com>
2022-08-09 18:32:57 +0000

    create django admin site

--- home/models.py
+++ home/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Portfolio(models.Model):
+    """
+    This class defines the Portfolio Model
+    that can be created by the user
+    """
+    name = models.CharField(max_length=9, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Asset(models.Model):
+    """
+    This class defines the Asset Model
+    to be added to the portfolio by the user
+    """
+    portfolio_name = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    ticker = models.CharField(max_length=6)
+    quantity = models.DecimalField(max_digits=10, decimal_places=3)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    average_price = models.JSONField(blank=True, null=True)
+    usd_spent = models.DecimalField(max_digits=10, decimal_places=3)
+    usd_earned = models.DecimalField(max_digits=10, decimal_places=3)
+    added_to_portfolio = models.DateTimeField(auto_now_add=True)
+    pnl = models.DecimalField(max_digits=10, decimal_places=3, default='0.00')
+
+    class Meta:
+        ordering = ['-ticker']
+
+    def __repr__(self):
+        return self.ticker
