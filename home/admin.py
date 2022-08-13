06fbd8947e9d631908778151ffda595784afa053
BrunoDS <bruno.diego.x@hotmail.com>
2022-08-13 07:34:11 +0000

    Add models to admin page

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from home.models import Portfolio, Asset
-# Register your models here.
-admin.site.register(Portfolio)
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_filter = ('user', 'name', 'created_on')
+    list_display = ('name', 'user', 'created_on')
+    search_fields = ['name', 'user']
+
+
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('ticker', 'portfolio_user', 'portfolio_name', 'pnl',
+                    'quantity', 'added_to_portfolio')
+    search_fields = ['portfolio_name']
+    list_filter = ('portfolio_name', 'pnl', 'added_to_portfolio')
+
+    def portfolio_user(self, instance):
+        return instance.portfolio_name.user
--- home/models.py
+++ home/models.py
@@ -9,6 +9,7 @@ class Portfolio(models.Model):
     that can be created by the user
     """
     name = models.CharField(max_length=9, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
 
