145ed98e63958b5dc5dcfca6de0db02d62adb886
BrunoDS <bruno.diego.x@hotmail.com>
2022-08-18 11:30:11 +0000

    Create register page

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import PortfoliosList, CustomLoginView, home_page
+from .views import PortfoliosList, CustomLoginView, RegisterPage, home_page
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('portfolios/', PortfoliosList.as_view(), name='portfolios'),
     path('', home_page, name='home-page'),
--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.views.generic.edit import FormView
 from django.http import HttpResponse
-from .models import Portfolio, Asset
 from django.urls import reverse_lazy
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
+from .models import Portfolio, Asset
 
 
 class CustomLoginView(LoginView):
@@ -14,11 +18,31 @@ class CustomLoginView(LoginView):
     '''
     template_name = 'home/login.html'
     fields = "__all__"
-    redirect_authenticated_user = False
+    redirect_authenticated_user = True
 
     def get_success_url(self):
         return reverse_lazy('portfolios')
 
+
+class RegisterPage(FormView):
+    '''
+    Class for the view of the register page
+    '''
+    template_name = 'home/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('portfolios')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **hwargs):
+        if self.request.user.is_authenticated:
+            return redirect('portfolios')
+        return super(RegisterPage, self).get(*args, **hwargs)
 
 
 class PortfoliosList(LoginRequiredMixin, generic.ListView):
