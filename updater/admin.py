69c3da217d76937937350971b2485847065c341c
BrunoDS <bruno.diego.x@hotmail.com>
2022-10-08 08:59:50 +0000

    Add model register to admin file

--- updater/admin.py
+++ updater/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from updater.models import Crypto
+
+@admin.register(Crypto)
+class CryptoAdmin(admin.ModelAdmin):
+    list_display = ('coins', 'timestamp')
+    search_fields = ['coins']
+    list_filter = ('coins', 'timestamp')
+    
