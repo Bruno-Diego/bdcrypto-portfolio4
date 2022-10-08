1a8a2cf6ff741ef3b06c9670b0d3a7ed14dff0e1
BrunoDS <bruno.diego.x@hotmail.com>
2022-10-08 08:51:54 +0000

    Add a model to store data from API

--- updater/models.py
+++ updater/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from datetime import datetime 
+
+class Crypto(models.Model):
+    """
+    This is a model to store the JSON data
+    sent from the API to update and validade the coins
+    """
+    coins = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.timestamps = datetime.utcnow()
+        return super(Crypto, self).save(*args, **kwargs)
