39dd4ffaf787f4b288152d8ccff33dacc7808b4a
BrunoDS <bruno.diego.x@hotmail.com>
2022-11-27 17:24:19 +0000

    create forms to add and edit assets

--- home/forms.py
+++ home/forms.py
@@ -1,15 +1,26 @@
-from .models import Asset
 from django import forms
+from .models import Asset
+from .cryptoapi import update_coins
+
+coins_list = update_coins()
+
+SELECT_CRYPTO = []
+
+for coin in coins_list:
+    SELECT_CRYPTO.append((coin['symbol'], coin['name'] + ' is currently: $' + coin['price']))
 
 
 class AssetForm(forms.ModelForm):
     class Meta:
         model = Asset
-        fields = ('__all__')
-        exclude = ('portfolio_name',)
+        fields = ('symbol', 'quantity')
+        labels = {'symbol': ('Choose an asset to buy:')}
+        widgets = {
+            'symbol': forms.Select(attrs={'class':'input-field'}, choices=SELECT_CRYPTO),
+        }
+
 
 class AssetUpdateForm(forms.ModelForm):
-    quantity = forms.DecimalField(initial=0)
     class Meta:
         model = Asset
         fields = ('quantity',)
